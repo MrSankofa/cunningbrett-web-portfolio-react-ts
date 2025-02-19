5f7875fb87e8a700b892df43929e42baa22a74d9
brett.cunningham <brett.cunningham@sbevision.com>
2025-02-19 14:29:45 -0500

    wip(html): vast improvement in responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
 import './assets/css/App.css'
 import './assets/css/main.css'
+import './assets/css/plugins.css'
 // import Loader from "./components/Loader.tsx";
 import Header from "./components/Header.tsx";
 import ProjectCard from "./components/ProjectCard.tsx";
@@ -24,28 +25,62 @@ function App() {
             {/*<Loader />*/}
             <Header />
 
+            {/* Project Section */}
+            <section className="py-[100px]">
+                <div className="w-full mx-auto xxl:max-w-[1280px] xl:max-w-[1140px] lg:max-w-[1024px] md:max-w-[768px] sm:max-w-[640px] px-[3px]">
+                    {/* Section Heading */}
+                    <div className="sec-head mb-[80px]">
+                        <div className="flex justify-center">
+                            <div className="px-[15px] lg:w-[75%] w-full">
+                                <div className="text-center">
+                                    <h1 className="text-[70px] mb-[15px] font-[500]">Brett</h1>
+                                    <h4 className="font-[500] lg:text-[28px] text-[20px]">
+                                        Software <span className="text-main">Developer</span>
+                                    </h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
-            {/* Hero Section */}
-            <section className="py-[100px] text-center">
-                <h1 className="text-[70px] mb-[15px] font-[500] text-[#fff]">Brett</h1>
-                <h4 className="font-[500] lg:text-[28px] text-[20px] text-[#fff]">
-                    Software <span className="text-main">Developer</span>
-                </h4>
+                    {/* Project Cards Grid */}
+                    <div className="w-full mx-auto xxl:max-w-[1280px] xl:max-w-[1140px] lg:max-w-[1024px] md:max-w-[768px] sm:max-w-[640px] px-[3px] grid md:grid-cols-2 sm:grid-cols-1 items-center justify-center gap-10">
+                        <ProjectCard
+                            title="Frontend Work"
+                            imgSrc="img/demos/4.png"
+                            link="dark/home.html"
+                        />
+                        <ProjectCard
+                            title="Backend Work"
+                            imgSrc="img/demos/3.png"
+                            link="dark/coming-soon.html"
+                        />
+                    </div>
+                </div>
             </section>
 
-            {/* Project Section */}
-            <div className="grid md:grid-cols-2 sm:grid-cols-1 gap-10 px-6">
-                <ProjectCard
-                    title="Frontend Work"
-                    imgSrc="img/demos/4.png"
-                    link="dark/home.html"
-                />
-                <ProjectCard
-                    title="Backend Work"
-                    imgSrc="img/demos/3.png"
-                    link="dark/coming-soon.html"
-                />
-            </div>
+
+
+            {/*/!* Hero Section *!/*/}
+            {/*<section className="py-[100px] text-center">*/}
+            {/*    <h1 className="text-[70px] mb-[15px] font-[500] text-[#fff]">Brett</h1>*/}
+            {/*    <h4 className="font-[500] lg:text-[28px] text-[20px] text-[#fff]">*/}
+            {/*        Software <span className="text-main">Developer</span>*/}
+            {/*    </h4>*/}
+            {/*</section>*/}
+
+            {/*/!* Project Section *!/*/}
+            {/*<div className="grid md:grid-cols-2 sm:grid-cols-1 gap-10 px-6">*/}
+            {/*    <ProjectCard*/}
+            {/*        title="Frontend Work"*/}
+            {/*        imgSrc="img/demos/4.png"*/}
+            {/*        link="dark/home.html"*/}
+            {/*    />*/}
+            {/*    <ProjectCard*/}
+            {/*        title="Backend Work"*/}
+            {/*        imgSrc="img/demos/3.png"*/}
+            {/*        link="dark/coming-soon.html"*/}
+            {/*    />*/}
+            {/*</div>*/}
 
             <Footer />
         </div>
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -3,26 +3,53 @@ import '../assets/css/style.css'
 interface ProjectCardProps {
     title: string;
     imgSrc: string;
-    link: string
+    link: string;
 }
 
-const ProjectCard = ({link, imgSrc, title}: ProjectCardProps) => {
-
+const ProjectCard = ({ link, imgSrc, title }: ProjectCardProps) => {
     return (
-        <div className="card-item bg-dark border border-[#ffffff1a] rounded-30 p-10 py-[100px] px-[15px] md:px-[40px] lg:px-[50px] relative mt-[40px]">
-            <div className="relative p-10">
-                <div className="border border-[#aaa] rounded-t-md overflow-hidden">
-                    <a href={link} target="_blank" rel="noopener noreferrer">
-                        <img src={imgSrc} alt={title} />
+        <div className="card-item bg-dark border border-[#ffffff1a] rounded-30 py-[100px] px-[15px] md:px-[40px] lg:px-[50px] relative mt-[40px]">
+            <div className="relative p-[40px] z-3 mt-[40px]">
+                <div className="border border-[#aaa] rounded-t-md rounded-b-none overflow-hidden">
+                    <a href={link} target="_blank" rel="noopener noreferrer" className="z-3">
+                        <img src={imgSrc} alt={title} className="max-w-[83%] mx-auto" />
                     </a>
                 </div>
-                <div className="h-[30px] border border-gray-400 rounded-b-full"></div>
+                <div className="custom-width ml-[-40px] h-[30px] border border-gray-400 rounded-b-full z-3 relative left-[4%]"></div>
             </div>
-            <div className="text-center">
+            <div className="text-center z-3">
                 <h3 className="font-[500] md:text-[38px] sm:text-[24px]">{title}</h3>
             </div>
         </div>
     );
-}
+};
 
 export default ProjectCard;
+
+
+// interface ProjectCardProps {
+//     title: string;
+//     imgSrc: string;
+//     link: string
+// }
+//
+// const ProjectCard = ({link, imgSrc, title}: ProjectCardProps) => {
+//
+//     return (
+//         <div className="card-item bg-dark border border-[#ffffff1a] rounded-30 p-10 py-[100px] px-[15px] md:px-[40px] lg:px-[50px] relative mt-[40px]">
+//             <div className="relative p-10">
+//                 <div className="border border-[#aaa] rounded-t-md overflow-hidden">
+//                     <a href={link} target="_blank" rel="noopener noreferrer">
+//                         <img src={imgSrc} alt={title} />
+//                     </a>
+//                 </div>
+//                 <div className="h-[30px] border border-gray-400 rounded-b-full"></div>
+//             </div>
+//             <div className="text-center">
+//                 <h3 className="font-[500] md:text-[38px] sm:text-[24px]">{title}</h3>
+//             </div>
+//         </div>
+//     );
+// }
+//
+// export default ProjectCard;
