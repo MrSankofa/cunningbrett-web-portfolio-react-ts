a03114e3569092cc48794dee6c5c6ebfb2561703
brett.cunningham <brett.cunningham@sbevision.com>
2025-02-19 12:30:21 -0500

    add(html): jquery scripts

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,15 @@ function App() {
 
   return (
     <>
-      
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        
+
       </div>
-      
+
+
     </>
   )
 }
